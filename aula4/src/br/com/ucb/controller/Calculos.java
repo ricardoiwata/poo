bb7b1246cd29ee388b290a333c01fc68f73ff538
ricardoiwata <iwataricardo1@gmail.com>
2023-08-30 20:49:51 -0300

    aula4

---
+++ aula4/src/br/com/ucb/controller/Calculos.java
@@ -0,0 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.com.ucb.controller;
+
+
+public class Calculos {
+    private double valor1;
+    private double valor2;
+
+    public double getValor1() {
+        return valor1;
+    }
+
+    public void setValor1(double valor1) {
+        this.valor1 = valor1;
+    }
+
+    public double getValor2() {
+        return valor2;
+    }
+
+    public void setValor2(double valor2) {
+        this.valor2 = valor2;
+    }
+    
+     public double somar() {
+        return this.valor1+this.valor2;
+    }
+     public double subtrair() {
+        return this.valor1-this.valor2;
+    }
+     public double dividir() {
+        return this.valor1/this.valor2;
+    }
+     public double multiplicar() {
+        return this.valor1*this.valor2;
+    }
+     public double porcentagem() {
+        return this.valor1*0.10;
+    }
+}
